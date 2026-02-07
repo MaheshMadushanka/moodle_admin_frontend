fdb4932d744c332c33dd4c54074dd8c5edb1cf6a
Vercel <vercel[bot]@users.noreply.github.com>
2026-02-07 18:52:35 +0000

    Enable Vercel Speed Insights on Your Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MoodleRoutes from './routes/MoodleRoutes'
 import { ThemeProvider } from './context/ThemeContext'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
     <ThemeProvider>
       <MoodleRoutes />
+      <SpeedInsights />
     </ThemeProvider>
   )
 }
