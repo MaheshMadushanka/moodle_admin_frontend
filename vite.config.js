2f2695a002d66f0e4b41fca33e316bcc73ab2cba
Mahesh Madushanka <your-email@example.com>
2026-02-16 23:55:01 +0530

    chunkSize waring

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false,
-    minify: "esbuild"
+    chunkSizeWarningLimit: 1000 // increase limit
   }
-});
+})
